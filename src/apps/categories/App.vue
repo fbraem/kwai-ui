0304ca8b2b3d0be839c55c9ffd19e551b4283947
fbraem <franky.braem@gmail.com>
2019-12-21 20:47:12 +0100

    Add container style

--- src/apps/categories/App.vue
+++ src/apps/categories/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="news">
     <router-view name="hero"></router-view>
-    <router-view></router-view>
+    <div class="container mx-auto p-4 lg:p-6">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
