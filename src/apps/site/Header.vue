64dc29f1ffa8e6a370375b08675e43baf115b47b
fbraem <franky.braem@gmail.com>
2020-05-16 17:39:30 +0200

    Use @ to resolve from root directory

--- src/apps/site/Header.vue
+++ src/apps/site/Header.vue
@@ -1,14 +1,14 @@
 <template>
   <!-- eslint-disable max-len -->
   <ImageHeader
-    :picture="require('../../site/images/judokwaikemzeke.jpg')"
+    :picture="require('@/site/images/judokwaikemzeke.jpg')"
     :pictures="pictures"
     title="Judokwai Kemzeke"
   >
     <div style="grid-area: hero-text; display: flex; flex-direction: column; justify-content: center;align-items: center">
       <div>
         <img
-          :src="require('../../site/images/logo2.png')"
+          :src="require('@/site/images/logo2.png')"
           style="width:121px;height:121px;" />
       </div>
       <div class="text-center">
