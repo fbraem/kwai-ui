13000229fadbd8e8389f3d242a43921528a2ef74
fbraem <franky.braem@gmail.com>
2020-05-31 20:05:12 +0200

    auth is now authentication

--- src/js/http.js
+++ src/js/http.js
@@ -18,7 +18,7 @@ export const http_api = http_auth
   .accept('application/vnd.api+json')
   .content('application/vnd.api+json')
   .catcher(401, async(_, request) => {
-    await store.dispatch('auth/refresh');
+    await store.dispatch('authentication/refresh');
     const token = store.state.authentication.access_token;
     return request.auth(token).replay().json();
   })
