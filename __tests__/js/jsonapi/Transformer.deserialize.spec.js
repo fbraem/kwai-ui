b6e205a02e541b7f6af18876f9ea35ced39f99ca
fbraem <franky.braem@gmail.com>
2020-06-07 22:31:50 +0200

    Using the first jest test scripts to refactor JSONAPI classes

---
+++ __tests__/js/jsonapi/Transformer.deserialize.spec.js
@@ -0,0 +1,203 @@
+import Model from '@/js/jsonapi/Model';
+import Transformer from '@/js/jsonapi/Transformer';
+
+class Book extends Model {
+  static type() {
+    return 'books';
+  }
+
+  static fields() {
+    return [
+      'title',
+    ];
+  }
+}
+
+const book1 = {
+  type: 'books',
+  id: '1',
+  attributes: {
+    title: 'Design Patterns: Elements of Reusable Object-Oriented Software'
+  }
+};
+
+const book2 = {
+  type: 'books',
+  id: '2',
+  attributes: {
+    title: 'Refactoring: Improving the Design of Existing Code'
+  }
+};
+
+const jsonBook = {
+  data: book1
+};
+
+const jsonBooks = {
+  data: [
+    book1,
+    book2,
+  ]
+};
+
+describe('Transformer - Deserialize with fields', () => {
+  test('It should create an model instance with fields', () => {
+    let transformer = new Transformer();
+    let book = transformer.deserialize(Book, jsonBook);
+    expect(book.title ===
+      'Design Patterns: Elements of Reusable Object-Oriented Software'
+    );
+  });
+
+  test('It should create an array with model instances with fields',
+    () => {
+      let transformer = new Transformer();
+      let books = transformer.deserialize(Book, jsonBooks);
+      expect(books.length === 2);
+      expect(books[0].title ===
+        'Design Patterns: Elements of Reusable Object-Oriented Software'
+      );
+    });
+});
+
+const authors = [
+  {
+    type: 'authors',
+    id: '1',
+    attributes: {
+      name: 'Erich Gamma'
+    }
+  },
+  {
+    type: 'authors',
+    id: '2',
+    attributes: {
+      name: 'Richard Helm'
+    }
+  },
+  {
+    type: 'authors',
+    id: '3',
+    attributes: {
+      name: 'Ralph Johnson'
+    }
+  },
+  {
+    type: 'authors',
+    id: '4',
+    attributes: {
+      name: 'John Vlissides'
+    }
+  },
+];
+book1.relationships = {
+  author: {
+    data: [
+      {
+        type: 'authors',
+        id: '1'
+      },
+      {
+        type: 'authors',
+        id: '2'
+      },
+      {
+        type: 'authors',
+        id: '3'
+      },
+      {
+        type: 'authors',
+        id: '4'
+      },
+    ]
+  }
+};
+jsonBook.included = authors;
+
+const authorBook2 = {
+  type: 'authors',
+  id: '5',
+  attributes: {
+    name: 'Martin Fowler'
+  }
+};
+book2.relationships = {
+  author: {
+    data: {
+      type: 'authors',
+      id: '5'
+    }
+  }
+};
+jsonBooks.included = [
+  ...authors,
+  authorBook2,
+];
+
+class Author extends Model {
+  static type() {
+    return 'authors';
+  }
+
+  static fields() {
+    return [
+      'name',
+    ];
+  }
+}
+
+class BookWithAuthor extends Model {
+  static type() {
+    return 'books';
+  }
+
+  static fields() {
+    return [
+      'title',
+    ];
+  }
+
+  static relationships() {
+    return {
+      author: Author
+    };
+  }
+
+  static computed() {
+    return {
+      authorCount(book) {
+        if (Array.isArray(book.author)) {
+          return book.author.length;
+        }
+        if (book.author) {
+          return 1;
+        }
+        return 0;
+      }
+    };
+  }
+}
+
+describe('Transformer - Deserialize with relationship', () => {
+  test('It should create a relationship as array and computed value',
+    () => {
+      let transformer = new Transformer();
+      let book = transformer.deserialize(BookWithAuthor, jsonBook);
+
+      expect(book.author).toBeInstanceOf(Array);
+      expect(book.title).toBe(
+        'Design Patterns: Elements of Reusable Object-Oriented Software'
+      );
+      expect(book.author.length).toBe(4);
+      expect(book.authorCount).toBe(4);
+    });
+
+  test('It should create a relationship as object and computed value',
+    () => {
+      let transformer = new Transformer();
+      let books = transformer.deserialize(BookWithAuthor, jsonBooks);
+      expect(books.length).toBe(2);
+      expect(books[1]?.author?.name).toBe('Martin Fowler');
+      expect(books[1]?.authorCount).toBe(1);
+    });
+});
---
+++ src/js/jsonapi/Transformer.js
@@ -0,0 +1,124 @@
+import Cache from './Cache';
+import Model from './Model';
+
+export default class Transformer {
+  constructor() {
+    this.cache = new Cache();
+  }
+
+  deserialize(model, { data, included = [] }) {
+    // When data is an array, deserialize all elements.
+    if (Array.isArray(data)) {
+      let models = [];
+      data.map(element => {
+        models.push(this.deserialize(model, { data: element, included }));
+      });
+      return models;
+    }
+
+    // Set all fields
+    let current = this.cache.getModel(model, data.id);
+    Object.entries(model.fields()).forEach((entry) => {
+      const [key, attr] = entry;
+      if (attr in data.attributes) {
+        current[attr] = data.attributes[attr];
+      }
+    });
+
+    // Set all relationships
+    let createRelated = (relation, relationModel) => {
+      let relatedData = included.find(o =>
+        o.type === relationModel.type() && o.id === relation.id
+      );
+      if (relatedData) {
+        return this.deserialize(
+          relationModel,
+          { data: relatedData, included }
+        );
+      }
+      return null;
+    };
+    if (data.relationships) {
+      Object.entries(model.relationships()).forEach(entry => {
+        const [relationName, relationModel] = entry;
+        if (relationName in data.relationships) {
+          if (Array.isArray(data.relationships[relationName].data)) {
+            current[relationName] = [];
+            data.relationships[relationName].data.forEach(relation => {
+              const relatedModel = createRelated(relation, relationModel);
+              if (relatedModel) {
+                current[relationName].push(relatedModel);
+              }
+            });
+          } else {
+            const relatedModel = createRelated(
+              data.relationships[relationName].data, relationModel
+            );
+            if (relatedModel) {
+              current[relationName] = relatedModel;
+            }
+          }
+        } else {
+          current[relationName] = null;
+        }
+      });
+    }
+
+    // Handle all computed values
+    // TODO: Reactivity?
+    const computed = model.computed();
+    Object.entries(computed).forEach((entry) => {
+      const [key, fn] = entry;
+      current[key] = fn(current);
+    });
+
+    return current;
+  }
+
+  serialize(modelInstance) {
+    if (!modelInstance instanceof Model) return null;
+
+    const modelClass = modelInstance.constructor;
+    let json = {
+      data: {
+        type: modelClass.type()
+      }
+    };
+    if (modelInstance.id) json.data.id = modelInstance.id;
+
+    // Handle all fields
+    json.data.attributes = Object.create(null);
+    Object.entries(modelClass.fields()).forEach(entry => {
+      const [key, attr] = entry;
+      if (attr in modelInstance) {
+        json.data.attributes[attr] = modelInstance[attr];
+      }
+    });
+
+    // Handle all relations
+    let relationships = Object.create(null);
+    Object.keys(modelClass.relationships()).forEach(relation => {
+      relationships[relation] = Object.create(null);
+      if (Array.isArray(modelInstance[relation])) {
+        relationships[relation].data = modelInstance[relation].map(
+          element => ({
+            type: element.constructor.type(),
+            id: element.id
+          })
+        );
+      } else {
+        if (modelInstance[relation]) {
+          relationships[relation] = {
+            data: {
+              type: modelInstance[relation].constructor.type(),
+              id: modelInstance[relation].id
+            }
+          };
+        }
+      }
+      json.data.relationships = relationships;
+    });
+
+    return json;
+  }
+};
