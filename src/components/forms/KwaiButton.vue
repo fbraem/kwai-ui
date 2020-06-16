400681408ca8ea277a3edde42adcdbde962ceee8
fbraem <franky.braem@gmail.com>
2020-06-16 20:13:59 +0200

    Add KwaiButton

---
+++ src/components/forms/KwaiButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button
+    class="red-button disabled:opacity-50 disabled:cursor-not-allowed"
+    :disabled="disabled"
+    :type="type"
+  >
+    <i
+      v-if="icon"
+      class="mr-2"
+      :class="icon"
+    >
+    </i>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    icon: {
+      type: String
+    },
+    type: {
+      type: String,
+      default: 'button'
+    }
+  }
+};
+</script>
