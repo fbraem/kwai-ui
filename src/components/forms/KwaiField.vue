7dfc7577d3871feb680c190abf50ce0d9afdfa25
fbraem <franky.braem@gmail.com>
2020-06-16 20:27:47 +0200

    Rename Field into KwaiField

--- src/components/forms/KwaiField.vue
+++ src/components/forms/KwaiField.vue
@@ -1,57 +1,53 @@
 <template>
-  <div class="flex flex-col">
-    <slot name="label">
-      <label
-        v-if="hasLabel"
-        class="block font-bold mb-2"
-        :class="{ 'text-red-600' : field.errors.length > 0 }"
-        :for="id"
-      >
-        {{ label }}
-        <i
-          v-if="field.required"
-          class="fas fa-asterisk text-red-700 text-xs align-top">
-        </i>
-      </label>
+  <ValidationProvider
+    :rules="rules"
+    :name="name"
+    class="mb-2"
+    v-slot="{ errors, required, valid, validated }"
+  >
+    <label
+      class="block font-bold mb-2"
+      :class="{ 'text-red-600': errors.length > 0 }"
+      :for="id"
+    >
+      <span>{{ label }}</span>
+      <span class="text-sm align-top">{{ required ? ' *': '' }}</span>
+    </label>
+    <slot :valid="valid || !validated" :required="required">
     </slot>
-    <slot></slot>
-    <slot name="error">
-      <div
-        v-for="(error, index) in field.errors"
-        class="text-red-600 text-sm italic"
-        :key="index"
-      >
-        {{ error }}
-      </div>
-    </slot>
-  </div>
+    <div
+      v-if="errors[0]"
+      class="text-red-600 text-sm italic"
+    >
+      {{ errors[0] }}
+    </div>
+  </ValidationProvider>
 </template>
 
 <script>
+import { ValidationProvider } from 'vee-validate';
+
 export default {
   props: {
-    field: {
-      type: Object,
-      required: true
+    rules: {
+      type: [Object, String],
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
     },
     id: {
       type: String,
       required: true
     },
-    label: {
+    name: {
       type: String
     }
   },
-  computed: {
-    hasLabel() {
-      return this.label && this.label.length > 0;
-    }
-  },
-  provide() {
-    return {
-      field: this.field,
-      id: this.id
-    };
+
+  components: {
+    ValidationProvider
   }
 };
 </script>
