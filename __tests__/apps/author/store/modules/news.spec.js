7560246f8a121642b4d69a912f302d934e52fd6c
fbraem <franky.braem@gmail.com>
2020-06-20 18:46:29 +0200

    Move state to describe scope

--- __tests__/apps/author/store/modules/news.spec.js
+++ __tests__/apps/author/store/modules/news.spec.js
@@ -2,8 +2,9 @@ import { initialize, getters, mutations } from '@/apps/author/store/modules/news
 import Story from '@/models/Story';
 
 describe('Author News Module', () => {
+  const state = initialize();
+
   test('setStories should set a story in the cache', () => {
-    const state = initialize();
     expect(state.count).toBe(0);
 
     mutations.setStories(state, {
@@ -16,13 +17,6 @@ describe('Author News Module', () => {
   });
 
   test('getStory should get a story from the cache', () => {
-    const state = initialize();
-    mutations.setStories(state, {
-      meta: {offset: 0, count: 1},
-      data: [
-        new Story('1'),
-      ]
-    });
     const story = getters.story(state)('1');
     expect(story.id).toBe('1');
   });
