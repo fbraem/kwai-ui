144f7ece77ec495cd1a97964b5d550658e767c55
fbraem <franky.braem@gmail.com>
2020-06-21 22:16:58 +0200

    Wrap with a div to allow adding classes

--- src/components/forms/KwaiField.vue
+++ src/components/forms/KwaiField.vue
@@ -1,27 +1,29 @@
 <template>
-  <ValidationProvider
-    :rules="rules"
-    :name="name"
-    class="mb-2"
-    v-slot="{ errors, required, valid, validated }"
-  >
-    <label
-      class="block font-bold mb-2"
-      :class="{ 'text-red-600': errors.length > 0 }"
-      :for="id"
+  <div>
+    <ValidationProvider
+      :rules="rules"
+      :name="name || label"
+      v-slot="{ errors, required, valid, validated }"
     >
-      <span>{{ label }}</span>
-      <span class="text-sm align-top">{{ required ? ' *': '' }}</span>
-    </label>
-    <slot :valid="valid || !validated" :required="required">
-    </slot>
-    <div
-      v-if="errors[0]"
-      class="text-red-600 text-sm italic"
-    >
-      {{ errors[0] }}
-    </div>
-  </ValidationProvider>
+      <label
+        v-if="label"
+        class="block font-bold mb-2"
+        :class="{ 'text-red-600': errors.length > 0 }"
+        :for="id"
+      >
+        <span>{{ label }}</span>
+        <span class="text-sm align-top">{{ required ? ' *': '' }}</span>
+      </label>
+      <slot :valid="valid || !validated" :required="required">
+      </slot>
+      <div
+        v-if="errors[0]"
+        class="text-red-600 text-sm italic"
+      >
+        {{ errors[0] }}
+      </div>
+    </ValidationProvider>
+  </div>
 </template>
 
 <script>
