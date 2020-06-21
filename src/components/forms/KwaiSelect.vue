2a0bb80d73f9837060e28057e883ca2955213f18
fbraem <franky.braem@gmail.com>
2020-06-21 22:17:24 +0200

    Make it work with vee-validate

--- src/components/forms/KwaiSelect.vue
+++ src/components/forms/KwaiSelect.vue
@@ -1,13 +1,15 @@
 <template>
   <!-- eslint-disable max-len -->
   <select
-    v-model="field.value"
+    :value="value"
     :id="id"
     class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 leading-thight rounded focus:outline-none focus:shadow-outline focus:border-gray-500"
-    :class="{ 'border-red-600' : field.errors.length > 0 }"
-    :required="field.required"
-    v-bind="$attrs">
-    <option value="" v-if="empty">{{ empty}}</option>
+    :class="{ 'border-red-600' : !valid }"
+    :required="required"
+    v-bind="$attrs"
+    @input="$emit('input', $event.target.value)"
+  >
+    <option value="" v-if="empty">{{ empty }}</option>
     <option v-for="(item, index) in items" :value="item.value" :key="index">
       {{ item.text }}
     </option>
@@ -16,13 +18,29 @@
 
 <script>
 export default {
-  inject: {
-    field: 'field',
-    id: 'id'
-  },
-  props: [
-    'empty',
-    'items',
-  ]
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    value: {
+      required: true
+    },
+    valid: {
+      type: Boolean,
+      required: true
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    items: {
+      required: true
+    },
+    empty: {
+      type: String,
+      default: ''
+    }
+  }
 };
 </script>
