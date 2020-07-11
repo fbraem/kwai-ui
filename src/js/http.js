cc30dbea996b1565161ead365feea32aef30b7ea
fbraem <franky.braem@gmail.com>
2020-07-11 20:11:14 +0200

    Check if a refresh_token resulted in a new access_token to avoid infinite loops

--- src/js/http.js
+++ src/js/http.js
@@ -17,9 +17,16 @@ export const http_auth = http.defer((w, url, options) => {
 export const http_api = http_auth
   .accept('application/vnd.api+json')
   .content('application/vnd.api+json')
-  .catcher(401, async(_, request) => {
+  .catcher(401, async(err, request) => {
     await store.dispatch('authentication/refresh');
+    if (!store.state.authentication.access_token) throw err;
+
     const token = store.state.authentication.access_token;
-    return request.auth(token).replay().json();
+    return request
+      .auth(token)
+      .replay()
+      .unauthorized(err => { throw err; })
+      .json()
+    ;
   })
 ;
