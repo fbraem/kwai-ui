990742f7fcaf973e6bd0974c9dd1878b1fa0ebe6
fbraem <franky.braem@gmail.com>
2020-08-12 22:15:20 +0200

    Use useAPI

--- src/apps/seasons/composables/useSeasons.js
+++ src/apps/seasons/composables/useSeasons.js
@@ -3,6 +3,7 @@ import URI from 'urijs';
 import { http_api } from '@/js/http';
 import Season from '@/models/Season';
 import Transformer from '@/js/jsonapi/Transformer';
+import { useAPI } from '@/js/useAPI';
 
 function createURI() {
   return new URI('seasons');
@@ -12,33 +13,21 @@ function createURI() {
  * State management for Season.
  */
 export default function useSeasons() {
-  const loading = ref(false);
   const saving = ref(false);
   const all = ref([]);
   const current = ref();
-  const error = ref();
 
-  /**
-   * Load all seasons
-   */
   async function load(reload = false) {
-    error.value = null;
     if (!reload && all.value.length > 0) return;
 
     const uri = createURI();
-    try {
-      loading.value = true;
-      const json = await http_api
-        .url(uri.href())
-        .get()
-        .json()
-      ;
-      all.value = (new Transformer()).deserialize(Season, json);
-    } catch (e) {
-      error.value = e;
-    } finally {
-      loading.value = false;
-    }
+    const json = await http_api
+      .url(uri.href())
+      .get()
+      .json()
+    ;
+    all.value = (new Transformer()).deserialize(Season, json);
+    return all.value;
   }
 
   /**
@@ -46,39 +35,29 @@ export default function useSeasons() {
    * @param id
    */
   async function read(id) {
-    error.value = null;
-
     // Don't read it again
     if (current.value?.id === id) return;
 
     // See if it was already loaded
     current.value = all.value.find((s) => s.id === id);
-    if (current.value) return;
+    if (current.value) return current.value;
 
     const uri = createURI();
     uri.segment(id);
 
-    try {
-      loading.value = true;
-      const json = await http_api
-        .url(uri.href())
-        .get()
-        .json()
-      ;
-      current.value = (new Transformer()).deserialize(Season, json);
-    } catch (e) {
-      current.value = null;
-      error.value = e;
-    } finally {
-      loading.value = false;
-    }
+    const json = await http_api
+      .url(uri.href())
+      .get()
+      .json()
+    ;
+    current.value = (new Transformer()).deserialize(Season, json);
+    return current.value;
   }
 
   /**
    * Save the season
    */
   async function save(season) {
-    error.value = null;
     const transformer = new Transformer();
     const uri = createURI();
     if (season.id) uri.segment(season.id);
@@ -87,17 +66,11 @@ export default function useSeasons() {
       .json(transformer.serialize(season))
     ;
 
-    try {
-      saving.value = true;
-      const res = await season.id ? api.patch() : api.post();
-      current.value = transformer.deserialize(Season, await res.json());
-      if (!season.id) all.value.push(current.value);
-    } catch (e) {
-      error.value = e;
-      throw e;
-    } finally {
-      saving.value = false;
-    }
+    const res = await season.id ? api.patch() : api.post();
+    current.value = transformer.deserialize(Season, await res.json());
+    if (!season.id) all.value.push(current.value);
+
+    return current.value;
   }
 
   /**
@@ -106,20 +79,16 @@ export default function useSeasons() {
   function reset() {
     all.value = [];
     current.value = null;
-    error.value = null;
-    loading.value = false;
   }
 
   return {
     all: computed(() => all.value),
     count: computed(() => all.value.length),
     current: computed(() => current.value),
-    error: computed(() => error.value),
-    isLoading: computed(() => loading.value),
     isSaving: computed(() => saving.value),
-    load,
-    read,
+    load: useAPI(load),
+    read: useAPI(read),
     reset,
-    save
+    save: useAPI(save)
   };
 };
