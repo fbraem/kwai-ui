57dd4b50c89d8c7be79111fc977f9fda1a3d00ba
fbraem <franky.braem@gmail.com>
2020-08-15 20:56:53 +0200

    Add w-full to div

--- src/components/Attributes.vue
+++ src/components/Attributes.vue
@@ -1,6 +1,6 @@
 <template>
   <!-- eslint-disable max-len -->
-  <div class="shadow overflow-hidden sm:rounded-lg">
+  <div class="w-full shadow overflow-hidden sm:rounded-lg">
     <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
       <slot name="title">
         <h3 class="text-lg leading-6 font-medium text-gray-900">
