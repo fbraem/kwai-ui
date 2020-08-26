7d03622679011cb320241e4bb6d5b64c14978480
fbraem <franky.braem@gmail.com>
2020-08-26 19:41:39 +0200

    The first service test

---
+++ __fixtures__/coaches.js
@@ -0,0 +1,17 @@
+export const coach = {
+  data: {
+    type: 'coaches',
+    id: '1',
+    attributes: {
+      description: 'Our first coach',
+      diploma: 'Coach A',
+      active: true
+    }
+  }
+};
+
+export const coaches =
+  {
+    data: [ coach.data ]
+  }
+;
