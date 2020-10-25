484c9b83b3d3c6c7d6944df7ea438c77b184b0bf
fbraem <franky.braem@gmail.com>
2020-10-25 15:10:02 +0100

    Use application service store

--- src/apps/club/App.vue
+++ src/apps/club/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <ImageHeader
-        v-if="application"
-        :title="application.title"
-        :pictures="pictures"
+      v-if="application"
+      :title="application.title"
+      :pictures="pictures"
     >
       <div v-html="application.description">
       </div>
@@ -12,36 +12,36 @@
     <div class="container mx-auto flex flex-col p-3">
       <div class="block">
         <HeaderLine tag="h4" content="Nieuws in de kijker" />
-        <Spinner v-if="$wait.is('news.browse')" />
-        <div v-if="storyCount === 0">
+        <Spinner v-if="newsStore.load.isRunning" />
+        <div v-if="newsStore.fullCount === 0">
           Momenteel is er geen belangrijk nieuws
         </div>
-        <div v-if="stories && stories.length > 0">
-          <NewsSlider :stories="stories" />
+        <div v-if="newsStore.all.length > 0">
+          <NewsSlider :stories="newsStore.all" />
         </div>
       </div>
       <div
-          v-if="application"
-          class="block mx-auto my-2"
+        v-if="application"
+        class="block mx-auto my-2"
       >
         <router-link
-            :to="moreNewsLink"
-            class="red-button"
+          :to="moreNewsLink"
+          class="red-button"
         >
           Meer nieuws
         </router-link>
       </div>
-      <Spinner v-if="$wait.is('pages.browse')" />
+      <Spinner v-if="pageStore.load.isRunning" />
       <div
-          class="block"
-          v-if="pageCount > 0"
+        class="block"
+        v-if="pageStore.fullCount > 0"
       >
         <HeaderLine tag="h4" content="Informatie" />
         <div class="flex flex-wrap">
           <div
-              v-for="page in pages"
-              :key="page.id"
-              class="p-3 w-full sm:w-1/2 lg:w-1/3"
+            v-for="page in pageStore.all"
+            :key="page.id"
+            class="p-3 w-full sm:w-1/2 lg:w-1/3"
           >
             <PageSummary :page="page" />
           </div>
@@ -52,97 +52,75 @@
 </template>
 
 <script>
-import newsStore from '@/apps/news/store';
-import pageStore from '@/apps/pages/store';
 import HeaderLine from '@/components/HeaderLine';
 import NewsSlider from '@/apps/news/components/NewsSlider';
 import PageSummary from '@/apps/pages/components/PageSummary';
 import ImageHeader from '@/components/ImageHeader';
 import Spinner from '@/components/Spinner';
+import useApplications from '@/site/composables/useApplications';
+// eslint-disable-next-line max-len
+import {providePageStore, usePageStore} from '@/apps/pages/composables/usePages';
+import {watch, reactive, computed} from '@vue/composition-api';
+import {provideNewsStore, useNewsStore} from '@/apps/news/composables/useNews';
 
 export default {
+  setup() {
+    const applicationStore = useApplications();
+
+    const application = computed(() => {
+      if (applicationStore.all.value) {
+        return applicationStore.all.value.find(a => a.name === 'club');
+      }
+      return null;
+    });
+
+    provideNewsStore();
+    const newsStore = useNewsStore();
+    providePageStore();
+    const pageStore = usePageStore();
+
+    watch(
+      () => application.value,
+      () => {
+        newsStore.load.run({
+          promoted: true,
+          application: application.value.id
+        });
+        pageStore.load.run({
+          application: application.value.id
+        });
+      }
+    );
+
+    const pictures = reactive({
+      '1024w': require('custom/club/images/header_lg.jpg'),
+      '768w': require('custom/club/images/header_md.jpg'),
+      '640w': require('custom/club/images/header_sm.jpg'),
+    });
+
+    const moreNewsLink = computed(() => {
+      return {
+        name: 'news.application',
+        params: {
+          application: application.value.id
+        }
+      };
+    });
+
+    return {
+      newsStore: reactive(newsStore),
+      pageStore: reactive(pageStore),
+      application,
+      pictures,
+      moreNewsLink
+    };
+  },
   components: {
     ImageHeader,
     HeaderLine,
     NewsSlider,
     PageSummary,
     Spinner
-  },
-  beforeCreate() {
-    this.$store.registerModule(['club'], { namespaced: true });
-    this.$store.registerModule(['club', 'news'], newsStore);
-    this.$store.registerModule(['club', 'page'], pageStore);
-  },
-  beforeDestroy() {
-    this.$store.dispatch('club/news/reset');
-    this.$store.unregisterModule(['club', 'news']);
-    this.$store.dispatch('club/page/reset');
-    this.$store.unregisterModule(['club', 'page']);
-    this.$store.unregisterModule(['club']);
-  },
-  computed: {
-    pictures() {
-      return {
-        '1024w': require('custom/club/images/header_lg.jpg'),
-        '768w': require('custom/club/images/header_md.jpg'),
-        '640w': require('custom/club/images/header_sm.jpg'),
-      };
-    },
-    application() {
-      // eslint-disable-next-line max-len
-      return this.$store.getters['applications/application']('club');
-    },
-    stories() {
-      return this.$store.state.club.news.cache[0];
-    },
-    storyCount() {
-      if (this.stories) return this.stories.length;
-      return 0;
-    },
-    pages() {
-      return this.$store.state.club.page.cache[0];
-    },
-    pageCount() {
-      if (this.pages) return this.pages.length;
-      return 0;
-    },
-    pageLink() {
-      return {
-        name: 'pages.read',
-        params: {
-          id: this.page.id
-        }
-      };
-    },
-    moreNewsLink() {
-      return {
-        name: 'news.category',
-        params: {
-          category: this.application.id
-        }
-      };
-    },
-  },
-  beforeRouteEnter(to, from, next) {
-    next(async(vm) => {
-      await vm.fetchData();
-    });
-  },
-  async beforeRouteUpdate(to, from, next) {
-    await this.fetchData();
-    next();
-  },
-  methods: {
-    async fetchData() {
-      await this.$store.dispatch('applications/load');
-      await this.$store.dispatch('club/news/load', {
-        application: this.application.id,
-        promoted: true
-      });
-      await this.$store.dispatch('club/page/load', {
-        application: this.application.id
-      });
-    }
   }
 };
 </script>
--- src/apps/judo/App.vue
+++ src/apps/judo/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <ImageHeader
-        v-if="application"
-        :title="application.title"
-        :pictures="pictures"
+      v-if="application"
+      :title="application.title"
+      :pictures="pictures"
     >
       <div v-html="application.description">
       </div>
@@ -12,36 +12,36 @@
     <div class="container mx-auto flex flex-col p-3">
       <div class="block">
         <HeaderLine tag="h4" content="Nieuws in de kijker" />
-        <Spinner v-if="$wait.is('news.browse')" />
-        <div v-if="storyCount === 0">
+        <Spinner v-if="newsStore.load.isRunning" />
+        <div v-if="newsStore.fullCount === 0">
           Momenteel is er geen belangrijk nieuws
         </div>
-        <div v-if="stories && stories.length > 0">
-          <NewsSlider :stories="stories" />
+        <div v-if="newsStore.all.length > 0">
+          <NewsSlider :stories="newsStore.all" />
         </div>
       </div>
       <div
-          v-if="application"
-          class="block mx-auto my-2"
+        v-if="application"
+        class="block mx-auto my-2"
       >
         <router-link
-            :to="moreNewsLink"
-            class="red-button"
+          :to="moreNewsLink"
+          class="red-button"
         >
           Meer nieuws
         </router-link>
       </div>
-      <Spinner v-if="$wait.is('pages.browse')" />
+      <Spinner v-if="pageStore.load.isRunning" />
       <div
-          class="block"
-          v-if="pageCount > 0"
+        class="block"
+        v-if="pageStore.fullCount > 0"
       >
         <HeaderLine tag="h4" content="Informatie" />
         <div class="flex flex-wrap">
           <div
-              v-for="page in pages"
-              :key="page.id"
-              class="p-3 w-full sm:w-1/2 lg:w-1/3"
+            v-for="page in pageStore.all"
+            :key="page.id"
+            class="p-3 w-full sm:w-1/2 lg:w-1/3"
           >
             <PageSummary :page="page" />
           </div>
@@ -52,97 +52,75 @@
 </template>
 
 <script>
-import newsStore from '@/apps/news/store';
-import pageStore from '@/apps/pages/store';
 import HeaderLine from '@/components/HeaderLine';
 import NewsSlider from '@/apps/news/components/NewsSlider';
 import PageSummary from '@/apps/pages/components/PageSummary';
 import ImageHeader from '@/components/ImageHeader';
 import Spinner from '@/components/Spinner';
+import useApplications from '@/site/composables/useApplications';
+// eslint-disable-next-line max-len
+import {providePageStore, usePageStore} from '@/apps/pages/composables/usePages';
+import {watch, reactive, computed} from '@vue/composition-api';
+import {provideNewsStore, useNewsStore} from '@/apps/news/composables/useNews';
 
 export default {
+  setup() {
+    const applicationStore = useApplications();
+
+    const application = computed(() => {
+      if (applicationStore.all.value) {
+        return applicationStore.all.value.find(a => a.name === 'judo');
+      }
+      return null;
+    });
+
+    provideNewsStore();
+    const newsStore = useNewsStore();
+    providePageStore();
+    const pageStore = usePageStore();
+
+    watch(
+      () => application.value,
+      () => {
+        newsStore.load.run({
+          promoted: true,
+          application: application.value.id
+        });
+        pageStore.load.run({
+          application: application.value.id
+        });
+      }
+    );
+
+    const pictures = reactive({
+      '1024w': require('custom/judo/images/header_lg.jpg'),
+      '768w': require('custom/judo/images/header_md.jpg'),
+      '640w': require('custom/judo/images/header_sm.jpg'),
+    });
+
+    const moreNewsLink = computed(() => {
+      return {
+        name: 'news.application',
+        params: {
+          application: application.value.id
+        }
+      };
+    });
+
+    return {
+      newsStore: reactive(newsStore),
+      pageStore: reactive(pageStore),
+      application,
+      pictures,
+      moreNewsLink
+    };
+  },
   components: {
     ImageHeader,
     HeaderLine,
     NewsSlider,
     PageSummary,
     Spinner
-  },
-  beforeCreate() {
-    this.$store.registerModule(['judo'], { namespaced: true });
-    this.$store.registerModule(['judo', 'news'], newsStore);
-    this.$store.registerModule(['judo', 'page'], pageStore);
-  },
-  beforeDestroy() {
-    this.$store.dispatch('judo/news/reset');
-    this.$store.unregisterModule(['judo', 'news']);
-    this.$store.dispatch('judo/page/reset');
-    this.$store.unregisterModule(['judo', 'page']);
-    this.$store.unregisterModule(['judo']);
-  },
-  computed: {
-    pictures() {
-      return {
-        '1024w': require('custom/judo/images/header_lg.jpg'),
-        '768w': require('custom/judo/images/header_md.jpg'),
-        '640w': require('custom/judo/images/header_sm.jpg'),
-      };
-    },
-    application() {
-      // eslint-disable-next-line max-len
-      return this.$store.getters['applications/application']('judo');
-    },
-    stories() {
-      return this.$store.state.judo.news.cache[0];
-    },
-    storyCount() {
-      if (this.stories) return this.stories.length;
-      return 0;
-    },
-    pages() {
-      return this.$store.state.judo.page.cache[0];
-    },
-    pageCount() {
-      if (this.pages) return this.pages.length;
-      return 0;
-    },
-    pageLink() {
-      return {
-        name: 'pages.read',
-        params: {
-          id: this.page.id
-        }
-      };
-    },
-    moreNewsLink() {
-      return {
-        name: 'news.category',
-        params: {
-          category: this.application.id
-        }
-      };
-    },
-  },
-  beforeRouteEnter(to, from, next) {
-    next(async(vm) => {
-      await vm.fetchData();
-    });
-  },
-  async beforeRouteUpdate(to, from, next) {
-    await this.fetchData();
-    next();
-  },
-  methods: {
-    async fetchData() {
-      await this.$store.dispatch('applications/load');
-      await this.$store.dispatch('judo/news/load', {
-        application: this.application.id,
-        promoted: true
-      });
-      await this.$store.dispatch('judo/page/load', {
-        application: this.application.id
-      });
-    }
   }
 };
 </script>
